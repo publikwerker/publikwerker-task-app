5afb96fc47975ab8ba03e2144b1644843490b308
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-10 11:34:49 -0700

    created task-manager file, created mongodb database on the localhose called task-manager

---
+++ task-manager/mongodb.js
@@ -0,0 +1,16 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, {useNewUrlParser:true}, (error, client)=>{
+  if (error){
+    return console.log(`${error}: An error occured`);
+  }
+  const db = client.db(databaseName);
+  db.collection('users').insertOne({
+    name: 'Jason',
+    age: 45
+  });
+});
