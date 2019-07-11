69e9b2e51330d64b850c45dcef9a6b27dd61c332
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-11 15:38:52 -0700

    completed exercises comparing callbacks to promises

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,33 +1,13 @@
-// setTimeout(() => {
-//   console.log(`Two seconds are up`)
-// }, 2000);
-
-// const names = ['Bob', 'Sue', 'Marv', 'Molly'];
-// const shortNames = names.filter((name) => {
-//   return name.length <= 4;
-// })
-
-// const geoCode = (address, callback) => {
-//   setTimeout(() => {
-//     const data = {
-//       latitude: 0,
-//       longitude: 0
-//     }
-//     callback(data)
-//   }, 2000)
-
-// }
-
-// geoCode('Portland', (data) => {
-//   console.log(data);
-// });
-
-const add = (x, y, callback) => {
-  setTimeout(()=> {
-    callback(x+y);
+const doWorkCallback = (callback) => {
+  setTimeout(()=>{
+    //callback('This is my error', undefined)
+    callback(undefined, [1,2,5])
   }, 2000)
 }
 
-add(1, 4, (sum) => {
-  console.log(sum) // should print: 5
-})
+doWorkCallback((error, result) => {
+  if (error){
+    return console.log(`${error}, there was an error`);
+  };
+  console.log(result)
+});
