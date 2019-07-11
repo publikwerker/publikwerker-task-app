aec7314b55a19cc0faa77767aecd4ca37ec0cd17
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-10 17:31:36 -0700

    began basic CRUD operations with mongodb

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,5 +1,4 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -9,8 +8,36 @@ MongoClient.connect(connectionURL, {useNewUrlParser:true}, (error, client)=>{
     return console.log(`${error}: An error occured`);
   }
   const db = client.db(databaseName);
-  db.collection('users').insertOne({
-    name: 'Jason',
-    age: 45
+  
+  // db.collection('users').findOne({_id: new ObjectID("5d26319cf8ef9a83b8d40082")}, (error, user) => {
+  //   if (error) {
+  //     return console.log(`${error}, couldn't find`);
+  //   }
+  //   console.log(user);
+  // });
+
+  // db.collection('users').find({
+  //   name: 'Jason'
+  // }).toArray((error, users)=>{
+  //   console.log(users)
+  // });
+
+  // db.collection('users').find({
+  //   name: 'Jason'
+  // }).count((error, count)=>{
+  //   console.log(count)
+  // })
+
+  db.collection('tasks').findOne({
+    _id: ObjectID("5d2634ea0ae51e8400139166")
+  }, (error, task) => {
+    console.log(task);
   });
+
+  db.collection('tasks').find({
+    completed: false
+  }).toArray((error, tasks) => {
+    console.log(tasks);
+  });
+
 });
