8dddb6ef29c54e044bb53c87e34a9f6e70007a11
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-13 12:40:04 -0700

    created endpoints for GET /users, to retrieve all users and GET /users/:id to get individual users

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -21,6 +21,31 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.get('/users', (req, res) => {
+  User.find({})
+  .then((users) => {
+    res.status(200).send(users);
+  })
+  .catch((err) => {
+    res.status(500).send(err);
+  });
+});
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  User.findById(id)
+  .then((user)=>{
+    if (!user){
+      res.status(404).send()
+    }
+    res.send(user);
+  })
+  .catch((err) => {
+    res.status(500).send(err);
+  })
+})
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
   task.save()
