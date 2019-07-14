192ac800263d1eaeb65bc9e3b823e914fd5ab415
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-14 14:25:18 -0700

    explored async/await when compared to promises

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,12 +1,16 @@
-const doWorkPromise = new Promise ((resolve, reject) => {
- setTimeout(() => {
-  //resolve([7,4,1])
-  reject(`Things went wrong`)
- }, 2000)
-})
+const add = (a,b) => {
+  return new Promise((resolve, reject ) => {
+    setTimeout(() => {
+      resolve(a+b);
+    }, 2000)
+  })
+}
 
-doWorkPromise.then((result)=>{
-  console.log(`Success!`, result)
-}).catch((error) => {
-  console.log(`${error}, Error`)
+add(1,2).then((sum) => {
+  console.log(sum)
+  return add(sum, 4)
+}).then((sum2)=>{
+  console.log(sum2)
+}).catch((err) => {
+  console.log(err)
 })
