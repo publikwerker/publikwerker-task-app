e95784f8f72e4e3ebb19e9bea6912b0abd8f6c8b
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-27 15:21:57 -0700

    added pagination to GET /tasks results using ?limit= &skip=

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -17,9 +17,25 @@ router.post('/tasks', auth, async (req, res) => {
   };
 });
 
+
+// GET /tasks?completed=true || false
+// GET /tasks?limit=10&skip=0
 router.get('/tasks', auth, async (req, res) => {
-  try {
-    await req.user.populate('tasks').execPopulate();
+  const match = {}
+  
+  if (req.query.completed){
+    match.completed = req.query.completed === 'true'
+  }
+
+ try {
+    await req.user.populate({
+      path: 'tasks',
+      match, 
+      options : {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip)
+      }
+    }).execPopulate();
     res.status(200).send(req.user.tasks);
   } catch (err) {
     res.status(500).send(err);
