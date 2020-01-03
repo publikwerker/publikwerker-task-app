9fd8ccbdf5f5a4d775aa761d485654ec88f5628c
Publikwerker <hello@publikwerks.com>
2020-01-03 14:19:44 -0800

    installed chalk

--- publik-note-app/app.js
+++ publik-note-app/app.js
@@ -1,6 +1,10 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
+const error = chalk.bold.red;
 
 const notes = getNotes();
+console.log(chalk.green('Success!'));
+console.log(error(notes));
 
-console.log(notes);
+console.log(chalk.inverse.blue(validator.isURL('jdawg@gmail.com')));
